ae8020b86cc6c0870d938aa099c6a0950adf7f49
iamram2510 <iamram2510@ieee.org>
2019-05-12 12:00:45 +0530

    added functionality to ask for creds

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # autogit
 A nodejs based cli which automates the creation of a local as well as remote repo on the code hosting websites
+
+[![Build Status](https://travis-ci.org/ram2510/autogit.svg?branch=master)](https://travis-ci.org/ram2510/autogit)
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 //========================================================================================
 /*                                                                                      *
  *                               Require node dependencies                              *
@@ -10,4 +12,49 @@ const chalk       =     require('chalk'),
 
 //########################################################################################
 
-console.log(require('./lib/files')('autogi'));
+// console.log(require('./lib/files')('autogi'));
+
+// the yargs starting script to accept arguments
+const argv =  require('yargs')
+              .usage('Usage: e.g $0 -r github -n example -a node')
+              .option('remote',{
+                alias:'r',
+                describe:"remote repo choice",
+                choices:['github', 'bitbucket', 'gitlab'],
+                nargs:1,
+                default:'github',
+                type:'string'
+              })
+              .option('name',{
+                alias:'n',
+                describe:'The name of the repo',
+                nargs:1,
+                type:'string'
+              })
+              .option('app',{
+                alias:'a',
+                describe:'The app you want to make',
+                nargs:1,
+                type:'string',
+                choices:['node(n)', 'express(e)'],
+              })
+              .help("h")
+              .alias('h', 'help')
+              .demandOption(['name'],'Please provide name to work with this tool')
+              .argv
+
+// we check here whether the directory exists or not
+
+var isFileExists = require('./lib/files')(argv.name);
+
+if(isFileExists){
+  console.log(chalk.red('Already a git repository!'));
+  process.exit();
+}else{
+  const run = async ()=>{
+    const {askRemoteCreds} = require('./lib/questions');
+    const creds = await askRemoteCreds(argv.remote);
+    console.log(creds);
+  }
+  run()
+}

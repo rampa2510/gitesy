74936b0d77de6756a136a6715bc35359db0ff9a5
iamram2510 <iamram2510@ieee.org>
2019-05-12 18:49:56 +0530

    Fixed bug for question test

--- tests/questions.test.js
+++ tests/questions.test.js
@@ -5,10 +5,14 @@
 //========================================================================================
 
 const { askRemoteCreds } = require('../lib/questions');
+const inquirer = require('inquirer');
+
 
 describe('Test for asking Questions', () => {
-  it('should return a function',()=>{
-    const creds = askRemoteCreds('github');
-    expect(creds).toBeDefined();
+  it('should return a function',async ()=>{
+    expect.assertions(1);
+    inquirer.prompt = jest.fn().mockResolvedValue({ username:'ram2510',password:'lol' });
+    await expect(askRemoteCreds('github')).resolves.toEqual({ username:'ram2510',password:'lol' });
   })
 })
+

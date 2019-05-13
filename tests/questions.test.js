83b7f12c81024b1710741f3cd384dfd7da212992
iamram2510 <iamram2510@ieee.org>
2019-05-13 16:30:45 +0530

    fixed questions test bug

--- tests/questions.test.js
+++ tests/questions.test.js
@@ -4,15 +4,38 @@
  *                                                                                      */
 //========================================================================================
 
-const { askRemoteCreds } = require('../lib/questions');
-const inquirer = require('inquirer');
+const { askRemoteCreds }  = require('../lib/questions');
+const { stdin }           = require('mock-stdin')
 
+
+// Mock stdin so we can send messages to the CLI
+
+let io = null
+beforeAll(() => (io = stdin()))
+afterAll(() => io.restore())
+
+const keys = {
+  up: '\x1B\x5B\x41',
+  down: '\x1B\x5B\x42',
+  enter: '\x0D',
+  space: '\x20'
+}
 
 describe('Test for asking Questions', () => {
-  it('should return a function',async ()=>{
-    expect.assertions(1);
-    inquirer.prompt = jest.fn().mockResolvedValue({ username:'ram2510',password:'lol' });
-    await expect(askRemoteCreds('github')).resolves.toEqual({ username:'ram2510',password:'lol' });
-  })
+  const sendKeystrokesForUsername = async () => {
+    io.send('r')
+    io.send(keys.enter)
+  }
+  const sendKeystrokesForPass = async () => {
+    io.send('r')
+    io.send(keys.enter)
+  }
+  setTimeout(() => sendKeystrokesForUsername().then(), 5)
+  setTimeout(() => sendKeystrokesForPass().then(), 7)
+  it('should be equal to the specidied object',async (done)=>{
+    const creds = await askRemoteCreds('github');
+    expect(creds).toStrictEqual({username:'r',password:'r'})
+   done()
+  })  
 })
 
